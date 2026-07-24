b6cffb5900aab2c283df0ad4e730ce01376f3f0d
Alexander Hildebrandt <alex@hilde.dev>
2026-07-24 14:03:10 +0200

    feat: help text for tasks

--- src/routes/(authenticated)/tasks/[task]/+page.ts
+++ src/routes/(authenticated)/tasks/[task]/+page.ts
@@ -1,13 +1,15 @@
 import type { PageLoad } from './$types';
 import { getApiClient } from '$lib/apiClient';
+import * as m from '$lib/paraglide/messages.js';
 import { handleApiLoad } from '$lib/utils/apiHelper';
 
 export const load: PageLoad = async ({ params, fetch }) => {
-  if (params.task === 'add') return { task: null };
+  if (params.task === 'add') return { task: null, help_text: m.schedule_info() };
 
   const client = getApiClient(fetch);
 
   return {
-    task: await handleApiLoad(client.api.tasks[':task'].$get({ param: { task: params.task } }))
+    task: await handleApiLoad(client.api.tasks[':task'].$get({ param: { task: params.task } })),
+    help_text: m.schedule_info()
   };
 };

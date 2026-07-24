460b8e8f730706394cb02e3dbbd2276876228bee
Alexander Hildebrandt <alex@hilde.dev>
2026-07-24 13:22:40 +0200

    feat: help text for expenses

--- src/routes/(authenticated)/balance/[entry]/+page.ts
+++ src/routes/(authenticated)/balance/[entry]/+page.ts
@@ -1,18 +1,20 @@
 import type { PageLoad } from './$types';
 import { getApiClient } from '$lib/apiClient';
+import * as m from '$lib/paraglide/messages.js';
 import { handleApiLoad } from '$lib/utils/apiHelper';
 
 export const load: PageLoad = async ({ url, params, fetch }) => {
   const purchaseId = url.searchParams.get('purchaseId');
 
   if (params.entry === 'add') {
-    return { entry: null, purchaseId };
+    return { entry: null, purchaseId, help_text: m.balance_info() };
   }
 
   const client = getApiClient(fetch);
 
   return {
     entry: await handleApiLoad(client.api.balance[':entry'].$get({ param: { entry: params.entry } })),
-    purchaseId
+    purchaseId,
+    help_text: m.balance_info()
   };
 };

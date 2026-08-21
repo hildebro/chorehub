bc87c72100440f67402ef19ca4b3a433599948ff
Alexander Hildebrandt <alex@hilde.dev>
2026-08-21 14:14:10 +0200

    feat: help text for dashboard

--- src/routes/(authenticated)/+page.ts
+++ src/routes/(authenticated)/+page.ts
@@ -1,5 +1,6 @@
 import type { PageLoad } from './$types';
 import { getApiClient } from '$lib/apiClient';
+import * as m from '$lib/paraglide/messages.js';
 import { handleApiLoad } from '$lib/utils/apiHelper';
 
 export const load: PageLoad = async ({ fetch }) => {
@@ -8,6 +9,7 @@ export const load: PageLoad = async ({ fetch }) => {
   return {
     shopping_item_count: await handleApiLoad(client.api.shopping.activeCount.$get()),
     last_purchase_date: await handleApiLoad(client.api.shopping.lastPurchaseDate.$get()),
-    user_debts: await handleApiLoad(client.api.balance.debts.$get())
+    user_debts: await handleApiLoad(client.api.balance.debts.$get()),
+    help_text: m.dashboard_help()
   };
 };

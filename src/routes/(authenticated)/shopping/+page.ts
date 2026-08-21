514bf53937f3e5920756490b8b07cc9b09f36d26
Alexander Hildebrandt <alex@hilde.dev>
2026-08-21 20:01:36 +0200

    feat: help text for shopping

--- src/routes/(authenticated)/shopping/+page.ts
+++ src/routes/(authenticated)/shopping/+page.ts
@@ -1,5 +1,6 @@
 import type { PageLoad } from './$types';
 import { getApiClient } from '$lib/apiClient';
+import * as m from '$lib/paraglide/messages.js';
 import { handleApiLoad } from '$lib/utils/apiHelper';
 
 export const load: PageLoad = async ({ fetch }) => {
@@ -7,6 +8,7 @@ export const load: PageLoad = async ({ fetch }) => {
 
   return {
     activeCategories: await handleApiLoad(client.api.shopping.categoriesWithActiveItems.$get()),
-    hasNoCategories: await handleApiLoad(client.api.shopping.hasNoCategories.$get())
+    hasNoCategories: await handleApiLoad(client.api.shopping.hasNoCategories.$get()),
+    help_text: m.shopping_help()
   };
 };

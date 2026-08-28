5d3631eb194ba21d668cd232edb7d6b6c4514f6a
Alexander Hildebrandt <alex@hilde.dev>
2026-08-28 12:19:41 +0200

    feat: replace argon package

--- src/lib/server/db/crypto.ts
+++ src/lib/server/db/crypto.ts
@@ -1,16 +1,23 @@
-import * as argon2 from 'argon2';
+import { argon2id, argon2Verify } from 'hash-wasm';
 
 export const hashPassword = async (password: string): Promise<string> => {
-  const hashingOptions = {
-    type: argon2.argon2id,
-    memoryCost: 65536, // 64 MB (passed in kilobytes)
-    timeCost: 3,       // Number of iterations
-    parallelism: 1    // Number of threads to use
-  };
+  const salt = new Uint8Array(16);
+  crypto.getRandomValues(salt);
 
-  return argon2.hash(password, hashingOptions);
+  return argon2id({
+    password: password,
+    salt: salt,
+    memorySize: 65536, // 64 MB
+    iterations: 3,     // timeCost
+    parallelism: 1,    // number of threads
+    hashLength: 32,    // standard output byte length
+    outputType: 'encoded' // Returns standard $argon2id$... string
+  });
 }
 
 export const verifyPassword = async (password: string, hashedPassword: string): Promise<boolean> => {
-  return argon2.verify(hashedPassword, password);
+  return argon2Verify({
+    password: password,
+    hash: hashedPassword
+  });
 }
